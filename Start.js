6ee511055f5a797e773e1ff765baa5684cf5c883
MarkoVVV <93085162+MarkOff@users.noreply.github.com>
2021-11-04 19:13:32 +0300

    Update and rename README.md to Start.js

---
+++ Start.js
@@ -0,0 +1,29 @@
+# FreeCodeCamp
+Traning JS
+ 
+Tusck№44
+// Setup
+const myArray = [["John", 23], ["cat", 2]];
+myArray.push(['dog',3]);
+// Only change code below this line
+console.log(myArray);
+
+
+Tusck№45
+// Setup
+const myArray = [["John", 23], ["dog", 3]];
+let removedFromMyArray = myArray.shift();
+// Only change code below this line
+console.log(myArray);
+
+Tusck№46
+// Setup
+const myArray = [["John", 23], ["dog", 3]];
+myArray.shift();
+myArray.unshift(["Paul",35])
+// Only change code below this line
+console.log(myArray);
+
+Tusck№47
+const myList = [['egg',10],['wather bottles',5],['chees',1 ],['lemons',2],['chocolate',4]];
+console.log(myList);
