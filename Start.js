5be2c19a38bede813669e4629c475ecdb0876c7f
MarkoVVV <93085162+MarkOff@users.noreply.github.com>
2021-11-11 14:37:22 +0300

    Update Start.js

--- Start.js
+++ Start.js
@@ -1,22 +1,28 @@
 # FreeCodeCamp
 Traning JS
  
-Tusck№44
+Task№44
 // Setup
 const myArray = [["John", 23], ["cat", 2]];
 myArray.push(['dog',3]);
 // Only change code below this line
 console.log(myArray);
 
+Task№45
+// Setup
+const myArray = [["John", 23], ["cat", 2]];
+const removedFromMyArray = myArray.pop();
+// Only change code below this line
+console.log(removedFromMyArray);
 
-Tusck№45
+Task№46
 // Setup
 const myArray = [["John", 23], ["dog", 3]];
 let removedFromMyArray = myArray.shift();
 // Only change code below this line
 console.log(myArray);
 
-Tusck№46
+Task47
 // Setup
 const myArray = [["John", 23], ["dog", 3]];
 myArray.shift();
@@ -24,6 +30,20 @@ myArray.unshift(["Paul",35])
 // Only change code below this line
 console.log(myArray);
 
-Tusck№47
+Task№48
 const myList = [['egg',10],['wather bottles',5],['chees',1 ],['lemons',2],['chocolate',4]];
 console.log(myList);
+
+Task№49
+ function reusableFunction(){
+   console.log("Hi World");
+ };
+ reusableFunction();
+
+Task№50
+function functionWithArgs(one, two) {
+  console.log(one + two);
+  console.log(one + two);
+};
+functionWithArgs(1,2);
+functionWithArgs(7,9);
