f1445f0272732efe45f3ddfb814bb8b429b79aaa
MarkoVVV <93085162+MarkOff@users.noreply.github.com>
2021-11-12 21:09:43 +0300

    Update Start.js

--- Start.js
+++ Start.js
@@ -190,3 +190,55 @@ function compareEquality(a, b) {
 
 compareEquality(10, "10");
 console.log(compareEquality());
+
+Task№63
+// Setup
+function testNotEqual(val) {
+  if (val!=99) { // Change this line
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+testNotEqual(10);
+
+Task№64
+// Setup
+function testStrictNotEqual(val) {
+  if (val!==17) { // Change this line
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+testStrictNotEqual(10);
+
+Task№65
+function testGreaterThan(val) {
+  if (val>100) {  // Change this line
+    return "Over 100";
+  }
+
+  if (val>10) {  // Change this line
+    return "Over 10";
+  }
+
+  return "10 or Under";
+}
+
+testGreaterThan(10);
+
+Task№66
+function testGreaterOrEqual(val) {
+  if (val>=20) {  // Change this line
+    return "20 or Over";
+  }
+
+  if (val>=10) {  // Change this line
+    return "10 or Over";
+  }
+
+  return "Less than 10";
+}
+
+testGreaterOrEqual(10);
