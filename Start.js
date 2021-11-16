9e9dd0ab742cf6e727e1ff85bf8c67597ed7abc4
MarkoVVV <93085162+MarkOff@users.noreply.github.com>
2021-11-16 18:52:53 +0300

    Update Start.js

--- Start.js
+++ Start.js
@@ -393,3 +393,114 @@ golfScore(5, 4);
 console.log(golfScore(4,2 ));
 
 Task№76
+function caseInSwitch(val) {
+  let answer = "";
+  // Only change code below this line
+  switch(val) {
+    case 1: 
+    return "alpha";
+    break;
+    case 2: 
+    return "beta";
+    break;
+    case 3: 
+    return "gamma";
+    break;
+    case 4: 
+    return "delta";
+    break;
+  }
+
+  // Only change code above this line
+  return answer;
+}
+caseInSwitch(1);
+console.log(caseInSwitch(1));
+
+Task№77
+function switchOfStuff(val) {
+  let answer = "";
+  // Only change code below this line
+  switch (val) {
+    case "a":
+      return "apple";
+      break;
+    case "b":
+      return "bird";
+      break;
+    case "c":
+      return "cat";
+      break;
+    default:
+      return "stuff";
+      break;
+  }
+
+
+  // Only change code above this line
+  return answer;
+}
+
+switchOfStuff(1);
+console.log(switchOfStuff("a"));
+
+Task№78
+function sequentialSizes(val) {
+  let answer = "";
+  // Only change code below this line
+  switch (val) {
+    case 1:
+    case 2:
+    case 3:
+      return "Low";
+      break;
+    case 4:
+    case 5:
+    case 6:
+      return "Mid";
+      break;
+    case 7:
+    case 8:
+    case 9:
+    return 'High';
+    break;
+  }
+
+
+  // Only change code above this line
+  return answer;
+}
+
+sequentialSizes(1);
+ console.log(sequentialSizes(6))
+
+Task№79
+function chainToSwitch(val) {
+  let answer = "";
+  // Only change code below this line
+switch (val) {
+  case "bob":
+    return "Marley";
+    brake;
+  case 42:
+    return "The Answer";
+    break;
+  case 1:
+    return "There is no #1";
+    break;
+  case 99:
+    return "Missed me by this much!";
+    break;
+  case 7:
+    return "Ate Nine";
+    break;
+  default: "";
+}
+  // Only change code above this line
+  return answer;
+}
+
+chainToSwitch(7);
+console.log(chainToSwitch())
+
+Task№80
