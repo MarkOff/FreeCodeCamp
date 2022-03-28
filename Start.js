91bc65f9ab57f1c15d7d00534a1fd1621af19976
MarkoVVV <93085162+MarkOff@users.noreply.github.com>
2022-03-28 22:26:05 +0300

    Update Start.js

--- Start.js
+++ Start.js
@@ -1,5 +1,69 @@
 # FreeCodeCamp
 Traning JS
+Task№01
+//qwerttrty
+/*qwertt
+qwert*/
+
+Task№02
+var myName;
+
+Task№03
+var a;
+a= 7;
+
+Task№04
+var a;
+a = 7;
+var b;
+b = a;
+
+Task№05
+var a = 9;
+
+Task№06
+var myFirstName = 'Vladislav';
+var myLastName = 'MarkoV';
+
+Task№07
+// Only change code below this line
+var a = 5;
+var b = 10;
+var c = "I am a";
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+Task№08
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+Task№09
+let catName = "Oliver";
+let catSound = "Meow!";
+
+Task№10
+const FCC = "freeCodeCamp"; // Change this line
+let fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(FCC, fact); // Change this line
+
+
+
+
+
+
+
+
 
 Task№44
 // Setup
