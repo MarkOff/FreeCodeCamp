203fe7a319932958b4839d2e0705317e2e3dfb85
MarkoVVV <93085162+MarkOff@users.noreply.github.com>
2022-08-24 17:59:13 +0300

    Update Start.js

--- Start.js
+++ Start.js
@@ -1,1 +1,85 @@
-hi
+Task №1
+//Hi World
+/*Hi
+World*/
+
+Task №2
+var myName;
+
+Task №3
+var a;
+var a = 7;
+
+Task №4
+var a;
+a = 7;
+var b;
+b = a;
+
+Task №5
+var a = 9;
+
+Task №6
+var myFirstName = 'Vladislav';
+var myLastName = 'Markov';
+
+Task №7
+var a = 5;
+var b = 10;
+var c = 'I am a';
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+Task №8
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+Task №9
+let catName = "Oliver";
+let catSound = "Meow!";
+
+Task №10
+const FCC = "freeCodeCamp"; 
+let fact = "is cool!"; 
+fact = "is awesome!";
+console.log(FCC, fact); 
+
+Task №11
+const sum = 10 + 10;
+
+Task №12
+const difference = 45 - 33;
+
+Task №13
+const product = 8 * 10;
+
+Task №14
+const quotient = 66 / 33;
+
+Task №15
+let myVar = 87;
+myVar++;
+console.log(myVar);
+
+Task №16
+let myVar = 11;
+myVar --;
+console.log(myVar)
+
+Task №17
+const myDecimal = 5.7;
+
+Task №18
+const product = 2.0 * 2.5;
+
+Task №19
+const quotient = 4.4 / 2.0; // Change this line
+
+Task №20
