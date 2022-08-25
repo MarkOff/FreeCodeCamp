081ed8b63c80a1e26081069cd9535ae84862589b
MarkoVVV <93085162+MarkOff@users.noreply.github.com>
2022-08-25 19:47:48 +0300

    Update Start.js

--- Start.js
+++ Start.js
@@ -83,3 +83,85 @@ Task №19
 const quotient = 4.4 / 2.0; // Change this line
 
 Task №20
+const remainder = 11 % 3;
+console.log(remainder);
+
+Task №21
+let a = 3;
+let b = 17;
+let c = 12;
+
+a += 12 ;
+b += 9;
+c += 7;
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+Task №22
+let a = 11;
+let b = 9;
+let c = 3;
+
+
+a -= 6 ;
+b -= 15 ;
+c -= 1;
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+Task №23
+let a = 5;
+let b = 12;
+let c = 4.6;
+
+
+a *= 5 ;
+b *= 3;
+c *= 10;
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+Task №24
+let a = 48;
+let b = 108;
+let c = 33;
+
+
+a /= 12;
+b /= 4;
+c /= 11;
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+Task №25
+const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; 
+console.log(myStr);
+
+Task №26
+const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+console.log(myStr);
+
+Task №27
+const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
+
+Task №28
+const myStr = 'This is the start. '+'This is the end.'; 
+console.log(myStr);
+
+Task №29
+let myStr = 'This is the first sentence. ';
+myStr += 'This is the second sentence.';
+console.log(myStr);
+
+Task №30
+const myName = 'Vlad';
+const myStr = 'My name is'+ myName + 'and I am well!';
+console.log(myStr);
