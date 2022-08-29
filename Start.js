6368640cdd0b36a17b0a89a56c5017a6aec5d759
MarkoVVV <93085162+MarkOff@users.noreply.github.com>
2022-08-29 15:20:05 +0300

    Update Start.js

--- Start.js
+++ Start.js
@@ -346,15 +346,117 @@ console.log(processed);
 Task №57
 function nextInLine(arr, item) {
   arr.push(item);
-  const removed = arr.shift();
-  return removed;
+  var sum = arr.shift();
+  return sum;
 }
 
 const testArr = [1, 2, 3, 4, 5];
-var sum = nextInLine([2], 1);
-console.log(sum)
+console.log("Before: " + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 6));
+console.log("After: " + JSON.stringify(testArr));
 
-Display code
- console.log("Before: " + JSON.stringify(testArr));
- console.log(nextInLine(testArr, 6));
- console.log("After: " + JSON.stringify(testArr));
+//FUNCTION + TRUE OR FALSE
+
+Task №58
+function welcomeToBooleans() {
+  var trueOrFalse = true;
+  return trueOrFalse; 
+}
+console.log(welcomeToBooleans());
+
+Task №59
+function trueOrFalse(wasThatTrue) {
+ if (wasThatTrue) {
+   return 'Yes, that was true';
+ }
+  return 'No, that was false';
+}
+console.log(trueOrFalse(true));
+console.log(trueOrFalse(false));
+
+Task №60
+function testEqual(val) {
+  if (val == 12) { 
+    return "Equal";
+  }
+  return "Not Equal";
+}
+console.log(testEqual(12));
+
+Task №61
+function testStrict(val) {
+  if (val === 7) { 
+    return "Equal";
+  }
+  return "Not Equal";
+}
+console.log(testStrict(7));
+
+Task №62
+function compareEquality(a, b) {
+  if (a === b) { 
+    return "Equal";
+  }
+  return "Not Equal";
+}
+console.log(compareEquality(10, 10));
+
+Task №63
+function testNotEqual(val) {
+  if (val != 99) { 
+    return "Not Equal";
+  }
+  return "Equal";
+}
+console.log(testNotEqual('99', 99));
+
+Task №64
+function testStrictNotEqual(val) {
+  if (val !== 17) { 
+    return "Not Equal";
+  }
+  return "Equal";
+}
+console.log(testStrictNotEqual(17));
+
+Task №65
+function testGreaterThan(val) {
+  if (val > 100) {  
+    return "Over 100";
+  }
+  if (val > 10) {  
+    return "Over 10";
+  }
+  return "10 or Under";
+}
+console.log(testGreaterThan(400));
+
+Task №66
+function testGreaterOrEqual(val) {
+  if (val >= 20) {  
+    return "20 or Over";
+  }
+  if (val >= 10) { 
+    return "10 or Over";
+  }
+  return "Less than 10";
+}
+console.log(testGreaterOrEqual(4));
+
+Task №67
+function testLessThan(val) {
+  if (val < 25) {  // Change this line
+    return "Under 25";
+  }
+
+  if (val < 55) {  // Change this line
+    return "Under 55";
+  }
+
+  return "55 or Over";
+}
+console.log(testLessThan(55);
+            
+Task №68
+            
+
