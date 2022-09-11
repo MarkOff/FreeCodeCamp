0a0a06aaf77bc2570c964d10843124b3ef3ab2f3
MarkoVVV <93085162+MarkOff@users.noreply.github.com>
2022-09-11 18:26:32 +0300

    Update Start.js

--- Start.js
+++ Start.js
@@ -994,7 +994,68 @@ do{
 console.log(i);
 
 Task №103
-//надо удалить, это с другого занятие 
-}while (i < 10) {
+function sum(arr, n) {
+  if (n <= 0) { 
+    return 0
+  } else {
+    return sum (arr, n - 1) + arr[n-1];
+  }
 }
-console.log(i);
+console.log(sum([1], 0));
+
+Task №104
+const contacts = [
+  {
+    firstName: "Akira",
+    lastName: "Laine",
+    number: "0543236543",
+    likes: ["Pizza", "Coding", "Brownie Points"],
+  },
+  {
+    firstName: "Harry",
+    lastName: "Potter",
+    number: "0994372684",
+    likes: ["Hogwarts", "Magic", "Hagrid"],
+  },
+  {
+    firstName: "Sherlock",
+    lastName: "Holmes",
+    number: "0487345643",
+    likes: ["Intriguing Cases", "Violin"],
+  },
+  {
+    firstName: "Kristian",
+    lastName: "Vos",
+    number: "unknown",
+    likes: ["JavaScript", "Gaming", "Foxes"],
+  },
+];
+
+function lookUpProfile(name, prop) {
+  for (let x = 0; x < contacts.length; x++) {
+    if (contacts[x].firstName === name) {
+      if (contacts[x].hasOwnProperty(prop)) {
+        return contacts[x][prop];
+      } else {
+        return "No such property";
+      }
+    }
+  }
+  return "No such contact";
+}
+
+console.log(lookUpProfile("Akira", "likes"));
+
+Task №105
+function randomFraction() {
+return Math.random();
+}
+console.log(randomFraction());
+
+Task №106
+function randomWholeNum() {
+  return Math.floor(Math.random() * 10);
+}
+console.log(randomWholeNum());
+
+Task №107
