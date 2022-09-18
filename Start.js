923eeec126e112c9f045c4812f27b94140f65e01
MarkoVVV <93085162+MarkOff@users.noreply.github.com>
2022-09-18 18:51:27 +0300

    Update Start.js

--- Start.js
+++ Start.js
@@ -1159,3 +1159,15 @@ const PI = freezeObj();
 console.log(PI);
 
 Task №4
+const magic = () => new Date();
+
+Task №5
+const myConcat = (arr1, arr2) => arr1.concat(arr2);
+
+console.log(myConcat([1, 2], [3, 4, 5]));
+
+Task №6
+const increment = (number, value = 1) => number + value ;
+console.log(increment(5));
+
+Task №7
